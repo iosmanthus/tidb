11cd5d5c1ddc1fd84495a1bcc3f33bfc6f4e77ec
ystaticy <y_static_y@sina.com>
2022-10-29 17:13:49 +0800

    fix_ddl_owner_election (#114)

--- store/gcworker/gc_worker.go
+++ store/gcworker/gc_worker.go
@@ -352,6 +352,10 @@ func (w *GCWorker) leaderTick(ctx context.Context) error {
 		zap.String("uuid", w.uuid),
 		zap.Uint64("safePoint", safePoint),
 		zap.Int("concurrency", concurrency))
+
+	ddl.RunWorker = false
+	logutil.Logger(ctx).Info("gc worker has been bootstrapped. Set ddl.RunWorker = false, to avoid trigger the election of ddl owner by createSession of deleteRange.")
+
 	go func() {
 		w.done <- w.runGCJob(ctx, safePoint, concurrency)
 	}()
@@ -642,6 +646,7 @@ func (w *GCWorker) runGCJob(ctx context.Context, safePoint uint64, concurrency i
 		failpoint.Return(errors.New("mock failure of runGCJoB"))
 	})
 	metrics.GCWorkerCounter.WithLabelValues("run_job").Inc()
+
 	usePhysical, err := w.checkUsePhysicalScanLock()
 	if err != nil {
 		return errors.Trace(err)
